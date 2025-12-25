a9b709a5b4e2ab722c358a9dc665145c88301432
Riyan <riyanmuqadar1203@gmail.com>
2025-12-25 14:16:25 +0500

    2m

--- vite.config.ts
+++ vite.config.ts
@@ -33,13 +33,13 @@ export default defineConfig(({ mode }) => ({
         start_url: "/stopwatch/",
         icons: [
           {
-            src: "icon-192.png",
+            src: "/stopwatch/icon-192.png",
             sizes: "192x192",
             type: "image/png",
             purpose: "any maskable"
           },
           {
-            src: "icon-512.png",
+            src: "/stopwatch/icon-512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable"
